c5e38d68f7160bda1d637a6f4eb5fcdc281e7356
René van Dorst <opensource@vdorst.com>
2020-07-28 15:50:55 +0200

    rules: added basic score.
    Signed-off-by: René van Dorst <opensource@vdorst.com>

--- src/big2rules.rs
+++ src/big2rules.rs
@@ -32,15 +32,7 @@ pub mod rules {
 		mask = 0b1000_0000_0111_1000;
 		if straigth & mask == mask { straigths += 1;};
 		
-		let mut flushs:u32 = 0;
-		//       2AKQ JT98 7654 3xxx
-		mask = 0x1111_1111_1111_1000;
-		for _ in 0..4 {
-			let cnt = (hand & mask).count_ones();
-			// println!("{:64b} {} {}", hand & mask, cnt, cnt / 5);
-			flushs += cnt / 5;
-			mask <<= 1;
-		}
+		let flushs = has_flush(hand);
 
 		let fullhouse = std::cmp::min(doubles, tripps) + std::cmp::min(doubles, quads) + std::cmp::min(tripps, quads);
 		println!("R{:x?} S{:16b} {:x} D{:x} T{:x} Q{:x} FH{:x} FL{:x}", ranks, straigth, straigths, doubles, tripps, quads, fullhouse, flushs);
@@ -74,6 +66,115 @@ pub mod rules {
 
 		return true;
 	}
+
+	pub fn is_flush(hand: u64) -> u64 {
+		let mut mask: u64 = 0x1111_1111_1111_1000;
+		for _ in 0..4 {
+			if (hand & mask).count_ones() == 5 { return (mask >> 12) & 0xF; }
+			mask <<= 1;
+		}
+		return 0;
+	}
+
+	pub fn has_flush(hand: u64) -> u8 {
+		let mut mask: u64 = 0x1111_1111_1111_1000;
+		let mut flushs: u8 = 0;
+		for _ in 0..4 {
+			if (hand & mask).count_ones() >= 5 { flushs += 1; }
+			mask <<= 1;
+		}
+		return flushs;
+	}
+
+	pub fn score_hand(hand: u64) -> u64 {
+		if is_valid_hand(hand) == false { return 0; }
+		// Score:
+		// 	
+	
+		// Score:
+		//	0xCRS = one 0x1.. to three cards 0x3..
+		// 	  ||+- Suit: Selected rank card shifted down.
+		//	  |+-- Rank: 3..F 3 = 3 and F = 2
+		//        +--- Card count: 1 = single, 2 = two, 3 = three
+		
+		//	Five cards
+		//		5/7 = straigt, 7 =straigth flush
+		//		R = card score 
+		// 		S = highst suit
+		
+		//		6 = flush
+		//		R = card score
+		//		S = suit
+		
+		//		8 = Full house
+		//		R = card score
+		//		S = suit
+		
+		//		9 = Quads
+		//		R = card score
+		//		S = suit
+		let card_cnt_hand: u64 = hand.count_ones().into();
+
+		if card_cnt_hand <= 3 {
+			for r in crate::big2rules::RANKS.iter() {
+				let rank: u64 = (*r).into();
+				let rankmask = (hand >> (rank << 2)) & 0xF;
+				let cnt: u64 = rankmask.count_ones().into();
+				if cnt == card_cnt_hand { return (cnt << 8) | (rank << 4) | rankmask; }
+			}
+			return 0;
+		}
+		
+		let mut straigth_bits = 0;
+		let mut single: u64 = 0;
+		let mut doubles: u64 = 0;
+		let mut tripples: u64 = 0;
+		let mut flush:	u64 = 0;
+
+		for r in crate::big2rules::RANKS.iter() {
+			let rank: u64 = (*r).into();
+			let rankmask = (hand >> (rank << 2)) & 0xF;
+			
+			if rankmask == 0 { 
+				straigth_bits = 0;
+				continue; 
+			}
+			
+			let cnt: u64 = rankmask.count_ones().into();
+
+			// Found Quads
+			if cnt == 4 { return 0x5400 | rank; }
+
+			let rs: u64 = rankmask | (rank << 4);
+			
+			flush |= rankmask;
+			
+			if cnt == 1 { single = rs; }
+			if cnt == 2 { doubles = rs; }	
+			if cnt == 3 { tripples = rs; }
+		}
+		
+		let is_flush: bool = flush.count_ones() == 1;
+
+		if straigth_bits == 5 && is_flush {
+			return 0x5600 | single;
+		}
+
+		if straigth_bits == 5 && !is_flush {
+			return 0x5000 | single;
+		}
+		
+		if is_flush {
+			return 0x5500 | single;
+		}
+
+		if tripples != 0 && doubles != 0 {
+			return 0x5400 | tripples;
+		}
+
+		println!("Unknown");
+		return 0;
+	}	
 }
 
 #[cfg(test)]
@@ -81,7 +182,7 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn rules_sizes() {
+	fn a_rules_sizes() {
 	        assert!(rules::is_valid_hand(0b1          ) == false, "1 invalid card");
 	        assert!(rules::is_valid_hand(0b1     << 12) == true);
 	        assert!(rules::is_valid_hand(0b11    << 12) == true);
@@ -125,5 +226,20 @@ mod tests {
 		assert!(rules::beter_hand(0b11111 << 12, 0b11     << 12) == false);
 		assert!(rules::beter_hand(0b11111 << 12, 0b111    << 12) == false);
 		assert!(rules::beter_hand(0b11111 << 12, 0b11111  << 12));
+	}
+	#[test]
+	fn b_rules_score_hand() {
+		assert!(rules::score_hand(0b1  << 12) == 0x131);
+		assert!(rules::score_hand(0b11 << 12) == 0x233);
+		// Select one 3 and one 4
+		assert!(rules::score_hand(0b11000 << 12) == 0);
+		assert!(rules::score_hand(0b11 << 12) < rules::score_hand(0b11 << 13));
+		// Select two 3 and one 4
+		assert!(rules::score_hand(0b111 << 12) == 0x337);
+		assert!(rules::score_hand(0b11100 << 12) == 0);
+		assert!(rules::score_hand(0b11 << 12) < rules::score_hand(0b11 << 13));
+		
+		// flush
+		assert!(rules::score_hand(0x11111 << 12) == 0);
 	}
 }
