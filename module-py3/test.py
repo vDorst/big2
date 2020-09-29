2fa0b84e41153a69967d580dac20977ba931f8f3
René van Dorst <opensource@vdorst.com>
2020-09-29 09:29:54 +0200

    py3lib: Benchmark playing game in python.
    Signed-off-by: René van Dorst <opensource@vdorst.com>

--- module-py3/test.py
+++ module-py3/test.py
@@ -4,30 +4,39 @@ import pybig2
 print (pybig2.score_hand(0xF1000))
 print (pybig2.score_hand(0x1000))
 
-BIG2 = pybig2.GameServer(8)
+def play_a_game():
+    BIG2 = pybig2.GameServer(8)
 
-BIG2.deal(None)
+    BIG2.deal(None)
 
-p = BIG2.turn()
-board = BIG2.board()
-print ("Turn: player %d, board: 0x%16x" % (p, board))
+    p = BIG2.turn()
+    board = BIG2.board()
+    #print ("Turn: player %d, board: 0x%16x" % (p, board))
 
-BIG2.action_play(p, 0x1000)
+    BIG2.action_play(p, 0x1000)
 
-p = BIG2.turn()
-board = BIG2.board()
-print ("Turn: player %d, board: 0x%16x" % (p, board))
+    p = BIG2.turn()
+    board = BIG2.board()
+    #print ("Turn: player %d, board: 0x%16x" % (p, board))
 
-BIG2.action_pass(p)
+    BIG2.action_pass(p)
 
-p = BIG2.turn()
-board = BIG2.board()
-print ("Turn: player %d, board: 0x%16x" % (p, board))
+    p = BIG2.turn()
+    board = BIG2.board()
+    #print ("Turn: player %d, board: 0x%16x" % (p, board))
 
-BIG2.action_play(p, 0x2000)
+    try:
+        BIG2.action_play(p, 0x2000)
+    except:
+        pass
 
-p = BIG2.turn()
-board = BIG2.board()
-print ("Turn: player %d, board: 0x%16x" % (p, board))
+    p = BIG2.turn()
+    board = BIG2.board()
+    #print ("Turn: player %d, board: 0x%16x" % (p, board))
 
-BIG2.action_pass(p)
+    BIG2.action_pass(p)
+
+if __name__ == "__main__":
+    import timeit
+    setup = "from __main__ import play_a_game"
+    print (timeit.timeit("play_a_game()", setup=setup, number=62500))
