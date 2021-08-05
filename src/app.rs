ec544946f6759b67560b29ef1fc02157fd2e44df
René van Dorst <opensource@vdorst.com>
2021-08-06 00:44:58 +0200

    basic big2 with egui
    Signed-off-by: René van Dorst <opensource@vdorst.com>

---
+++ src/app.rs
@@ -0,0 +1,108 @@
+use crate::{cards, widgets};
+use eframe::{egui, epi};
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
+pub struct TemplateApp {
+    cards_selected: u64,
+    want_pass: bool,
+}
+
+impl Default for TemplateApp {
+    fn default() -> Self {
+        Self {
+            cards_selected: 0,
+            want_pass: false,
+        }
+    }
+}
+
+impl epi::App for TemplateApp {
+    fn name(&self) -> &str {
+        "Big2 (Alpha)"
+    }
+
+    /// Called by the framework to load old app state (if any).
+    #[cfg(feature = "persistence")]
+    fn setup(
+        &mut self,
+        _ctx: &egui::CtxRef,
+        _frame: &mut epi::Frame<'_>,
+        storage: Option<&dyn epi::Storage>,
+    ) {
+        if let Some(storage) = storage {
+            *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
+        }
+    }
+
+    /// Called by the frame work to save state before shutdown.
+    #[cfg(feature = "persistence")]
+    fn save(&mut self, storage: &mut dyn epi::Storage) {
+        epi::set_value(storage, epi::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        let Self {
+            cards_selected: selected,
+            want_pass,
+        } = self;
+
+        // Examples of how to create different panels and windows.
+        // Pick whichever suits you.
+        // Tip: a good default choice is to just keep the `CentralPanel`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                egui::menu::menu(ui, "File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        frame.quit();
+                    }
+                });
+            });
+        });
+
+        // egui::SidePanel::left("side_panel").show(ctx, |ui| {
+        //     ui.heading("Side Panel");
+
+        //     ui.horizontal(|ui| {
+        //         ui.label("Write something: ");
+        //         ui.text_edit_singleline(label);
+        //     });
+
+        //     ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
+        //     if ui.button("Increment").clicked() {
+        //         *value += 1.0;
+        //     }
+
+        //     ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
+        //         ui.add(
+        //             egui::Hyperlink::new("https://github.com/emilk/egui/").text("powered by egui"),
+        //         );
+        //     });
+        // });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+
+            ui.add(cards::cards(selected, 0x8208_0342_3122_1000));
+            ui.add(widgets::buttons::button_pass(want_pass, false));
+            ui.add(widgets::buttons::button_pass(want_pass, true));
+
+            egui::warn_if_debug_build(ui);
+        });
+
+        if false {
+            egui::Window::new("Window").show(ctx, |ui| {
+                ui.label("Windows can be moved by dragging them.");
+                ui.label("They are automatically sized based on contents.");
+                ui.label("You can turn on resizing and scrolling if you like.");
+                ui.label("You would normally chose either panels OR windows.");
+            });
+        }
+    }
+}
---
+++ src/widgets/buttons.rs
@@ -0,0 +1,64 @@
+use eframe::egui::{self, Align2, Color32, Stroke, TextStyle};
+
+pub fn button_pass_ui(ui: &mut egui::Ui, selected: &mut bool, passed: bool) -> egui::Response {
+    let button_size = ui.spacing().interact_size.y * egui::vec2(3.0, 1.0);
+
+    let (rect, mut response) = ui.allocate_exact_size(button_size, egui::Sense::click());
+
+    if response.clicked() {
+        *selected = !*selected;
+        response.mark_changed(); // report back that the value changed
+    }
+
+    let select = *selected;
+    // 4. Paint!
+    // First let's ask for a simple animation from egui.
+    // egui keeps track of changes in the boolean associated with the id and
+    // returns an animated value in the 0-1 range for how much "on" we are.
+    // let how_on = ui.ctx().animate_bool(response.id, select);
+    // We will follow the current style by asking
+    // "how should something that is being interacted with be painted?".
+    // This will, for instance, give us different colors when the widget is hovered or clicked.
+
+    let mut visuals = *ui.style().visuals.widgets.style(&response);
+    visuals.bg_fill = Color32::WHITE;
+    visuals.fg_stroke = Stroke::new(1.0, Color32::BLACK);
+    if select {
+        visuals.bg_fill = Color32::LIGHT_GRAY;
+        // visuals.bg_stroke = self.visuals.selection.stroke;
+        visuals.fg_stroke = Stroke::new(2.0, Color32::BLUE);
+    }
+
+    // All coordinates are in absolute screen coordinates so we use `rect` to place the elements.
+    let rect = rect.expand(visuals.expansion);
+
+    ui.painter()
+        .rect(rect, 1.0, visuals.bg_fill, visuals.fg_stroke);
+
+    let col = match (passed, select) {
+        (true, _) => Color32::GRAY,
+        (false, false) => Color32::RED,
+        (false, true) => Color32::BLUE,
+    };
+    ui.painter().text(
+        rect.center(),
+        Align2::CENTER_CENTER,
+        "Pass",
+        TextStyle::Button,
+        col,
+    );
+    //.circle(center, 0.75 * radius, visuals.bg_fill, visuals.fg_stroke);
+
+    ui.label("hello")
+}
+
+// A wrapper that allows the more idiomatic usage pattern: `ui.add(...)`
+/// cards entry field with ability to toggle character hiding.
+///
+/// ## Example:
+/// ``` ignore
+/// ui.add(cards(&mut selected, &str value));
+/// ```
+pub fn button_pass<'a>(want_pass: &'a mut bool, passed: bool) -> impl egui::Widget + 'a {
+    move |ui: &mut egui::Ui| button_pass_ui(ui, want_pass, passed)
+}
